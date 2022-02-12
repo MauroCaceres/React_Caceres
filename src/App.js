3791955a9bceb45d3a5207a14be79f3590d24aeb
Mauro Caceres <caceres_mauro22789@outlook.com>
2022-02-12 13:03:00 -0300

    Entregable 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Proyecto Coder
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Mundo!</h1>
     </div>
   );
 }
